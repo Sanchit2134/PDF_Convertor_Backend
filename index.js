48773a775088266a6209c371eb34bffd8bd7c817
Sanchit Thakur <jarvus972001@gmail.com>
2024-04-28 19:30:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const docxToPdf = require('docx-pdf');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+//Storage create kar rahai hain
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    }
+  })
+  const upload = multer({ storage: storage })
+  
+  app.post('/convertor', upload.single('file'), (req, res, next)=> {
+    try{
+        if(!req.file){
+            return res.status(400).json({
+                message: 'Please upload a file'
+            })
+        }
+        //defining output file path
+        let outputFilePath = path.join(__dirname,"files",`${req.file.originalname}.pdf`)
+
+        //converting docx to pdf
+        docxToPdf(req.file.path , outputFilePath , (err,result)=>{
+            if(err){
+              console.log(err);
+              return res.status(500).json({
+                message: 'Error in converting file'
+              })
+            }
+            res.download(outputFilePath, ()=>{
+                console.log('File downloaded successfully');
+            })
+          });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'Internal server error',
+        
+        })
+    }
+  })
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
